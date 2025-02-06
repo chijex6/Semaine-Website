495189fc1242f1b42bbc54b059f25cb360b93c3e
chijex6 <embroconnect2@gmail.com>
2025-02-06 19:42:58 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import React from "react";
 import ReactDOM from "react-dom";
 import { AppRouter } from "./AppRouter";
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <AppRouter />
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById("root")
 )
